1765812186a206659af9f6e598f29055003b925d
pedritto19 <82061131+pedritto19@users.noreply.github.com>
2025-06-25 15:59:54 -0300

    feat: sector get

--- src/main/java/com/estapar/parking/model/entity/Sector.java
+++ src/main/java/com/estapar/parking/model/entity/Sector.java
@@ -1,21 +1,68 @@
 package com.estapar.parking.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.time.LocalTime;
 
 @Entity
 public class Sector {
 
     @Id
-    private String name;
+    private String name; // Ex: "A", "B", "C"
 
     private double basePrice;
     private int maxCapacity;
-
     private LocalTime openHour;
     private LocalTime closeHour;
-
     private int durationLimitMinutes;
 
     // Getters e Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public LocalTime getOpenHour() {
+        return openHour;
+    }
+
+    public void setOpenHour(LocalTime openHour) {
+        this.openHour = openHour;
+    }
+
+    public LocalTime getCloseHour() {
+        return closeHour;
+    }
+
+    public void setCloseHour(LocalTime closeHour) {
+        this.closeHour = closeHour;
+    }
+
+    public int getDurationLimitMinutes() {
+        return durationLimitMinutes;
+    }
+
+    public void setDurationLimitMinutes(int durationLimitMinutes) {
+        this.durationLimitMinutes = durationLimitMinutes;
+    }
 }
